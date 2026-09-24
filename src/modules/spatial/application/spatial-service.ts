51768b0b180a7b32a1d3e280536d936483247f32
em3rc0dTh <eduardo.farid@thradex.com>
2026-09-24 11:01:59 -0500

    fix(cluster): derive cluster extent only from 600 mm slots

--- src/modules/spatial/application/spatial-service.ts
+++ src/modules/spatial/application/spatial-service.ts
@@ -231,8 +231,7 @@ export class SpatialService {
           };
         }
 
-        const positionIds = new Set(clusterPositions.map((position) => position.id));
-        const positionRects = clusterPositions.map((position) => {
+        const rects = clusterPositions.map((position) => {
           const point = gridCoordinateToPoint(position.coordinate);
           return {
             x: point.x,
@@ -241,10 +240,6 @@ export class SpatialService {
             depth: TILE_SIZE_MM,
           };
         });
-        const physicalRackRects = racks
-          .filter((rack) => rack.positionId && positionIds.has(rack.positionId))
-          .map((rack) => rack.rect);
-        const rects = [...positionRects, ...physicalRackRects];
         const minX = Math.min(...rects.map((rect) => rect.x));
         const minY = Math.min(...rects.map((rect) => rect.y));
         const maxX = Math.max(...rects.map((rect) => rect.x + rect.width));
